802d9225aa3d2c43e1dd342e0d4f5bfa11293019
geeno <geeno.li@360zebra.com>
2020-02-01 16:59:33 +0800

    initial

---
+++ OdeFood/OdeToFood.Data/Repos/RestaurantsRepo.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data
+{
+	using M = Restaurants.Restaurant;
+	public class RestaurantsRepo : IRestaurant
+	{
+		public IEnumerable<M> GetRestaurants()
+			=> Enumerable.Range(1, 10).Select(
+				x => M.Make
+				(
+					x, "jack", "usa", CuisineType.IDs.ChuanCai
+				)).ToList();
+	}
+}
